1d0baf1fe050cc063c925e68dc7b038501311980
RealCR1 <wz892477341@outlook.com>
2018-03-31 00:14:50 +0800

    Adding SLList data structure.

--- bookSourceCode/Chapter1/1.3/SLList.java
+++ bookSourceCode/Chapter1/1.3/SLList.java
@@ -1,15 +1,42 @@
 public class SLList {
+	public class IntNode {
+		public int item;
+		public IntNode next;
+
+		public IntNode(int i, IntNode n) {
+			item = i;
+			next = n;
+		}
+	}
+
 	public IntNode first;
 
 	public SLList(int x) {
 		first = new IntNode(x, null);
 	}
 
-	public void addFirst(Int x) {
+	public void addFirst(int x) {
 		first = new IntNode(x, first);
 	}
 
 	public int getFirst() {
 		return first.item;
+	}
+
+	public void addLast(int x) {
+		IntNode p = first;
+		while (p.next != null) {
+			p = p.next;
+		}
+
+		p.next = new IntNode(x, null);
+
+	}
+
+	public int size(IntNode p) {
+		if (p.next == null) {
+			return 1;
+		}
+		return 1 + size(p.next);
 	}
 }
